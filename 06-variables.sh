82cb0c6ef21b348f77f900180ea73fb086a83119
siva <siva123@gmail.com>
2023-08-26 23:49:32 +0530

    hello-world script

--- 06-variables.sh
+++ 06-variables.sh
@@ -6,3 +6,9 @@ echo "please enter your username"
 read USERNAME 
 
 echo "username entered is:$USERNAME"
+
+echo "please entered your password"
+
+read PASSWORD
+
+echo "password entered is:$PASSWORD"
