296aada4478edb04b425d4f1796a06c14c9991f8
siva <siva123@gmail.com>
2023-08-26 23:30:35 +0530

    hello-world script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1: Hi $PERSON2 Good morning!"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: iam good, how are you $PERSON1"
