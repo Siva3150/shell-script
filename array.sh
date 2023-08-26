ad5feaa1b7e8080db8eada1f9ef86068238bbae0
siva <siva123@gmail.com>
2023-08-27 00:03:17 +0530

    hello-world script

--- array.sh
+++ array.sh
@@ -4,4 +4,8 @@ PERSONS=("Ramesh" "suresh" "sachin")
 
 echo "First person:${PERSONS[0]}"
 
+echo "First person:${PERSONS[1]}"
+
+echo "First person:${PERSONS[2]}"
+
 echo "First person:${PERSONS[@]}"
